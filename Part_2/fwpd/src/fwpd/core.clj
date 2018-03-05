f2ff3dd237dde7b4cbe5e8dc9a0bfa258975d16e
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-03-05 15:25:20 -0500

    finished exercises for chapter 4

--- Part_2/fwpd/src/fwpd/core.clj
+++ Part_2/fwpd/src/fwpd/core.clj
@@ -35,5 +35,63 @@
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
+; Exercises
 
-(pprint (glitter-filter 3 (mapify (parse (slurp filename)))))
+; 1. Turn the result of your giltter function into a list of names
+(defn get-names
+  [rows]
+  (map :name rows))
+; (get-names (glitter-filter 3 (mapify (parse (slurp filename)))))
+
+; 2. Write a function append, which will append a new suspect to your list of suspects
+(defn append
+  [suspect-list new-suspect]
+  (conj suspect-list new-suspect))
+
+; (pprint
+;   (append
+;    (mapify (parse (slurp filename)))
+;    {:name "Jordan"}))
+
+; 3. Write a function, validate, which will check that :name and :glitter-index are present when you append. The validate function should accept two arguments: a map of keywords to validating functions, similitar to conversions, and the record to be validated.
+(defn validate [keys m]
+ (every? (partial contains? m) keys))
+
+; (validate
+;   [:name :glitter-index] {:name "Jordan" :glitter-index 5})
+
+; We can now rewrite append to use validate
+; (defn append
+;   [suspect-list new-suspect]
+;   (if (validate [:name :glitter-index] new-suspect)
+;     (conj suspect-list new-suspect)
+;     (seq suspect-list)))
+  
+
+; Write a function that will take your list of maps and convert it back to a CSV string.  Use clojure.string/join
+
+; convert from map to csv
+(defn convertBack
+  [m]
+  (clojure.string/join "," (toVector m)))
+      
+(defn toVector
+  [m]
+  (conj []
+        (:name m) 
+        (:glitter-index m)))
+
+; a test
+; (convertBack
+;   {:name "Jordan" :glitter-index 5})
+
+; this could be written with let
+(def )
+  
+(println
+  (let [output
+        (mapify (parse (slurp filename)))]
+       (reduce str 
+        (map #(str % "\n")
+         (map convertBack output)))))
+  
