ce5919a4d95fa1d58ac73a86e52a9a80d2355963
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-03-27 18:57:24 -0400

    adding chapter 6 project files

---
+++ Part_2/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -0,0 +1,12 @@
+(ns the-divine-cheese-code.visualization.svg)
+
+(defn latlng->point
+  "Convert lat/lng map to comma-seperated string"
+  [latlng]
+  (str (:lat latlng) ", " (:lng latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join
+    " "
+    (map latlng->point locations)))
